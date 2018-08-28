4b7665aa1d0f19bf289426002ced48571ac4c02f
Alexander Fleischer <fleisch91gmail.com>
2018-08-29 00:31:00 +0200

    Finished form.

--- src/web/app.py
+++ src/web/app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, flash
+from flask_wtf import FlaskForm
+from wtforms import DecimalField
+from wtforms import validators
+
 app = Flask(__name__)
+app.secret_key = "wtf is this"
 
 WEIGHT_INCREMENT = 0.125
 WEIGHT_PRECISION = 2.5
 NUM_WEEKS = 12
+
+class RMForm(FlaskForm):
+    squat = DecimalField("squat", validators=[validators.DataRequired()])
+    bench = DecimalField("bench", validators=[validators.DataRequired()])
+    dl = DecimalField("dl", validators=[validators.DataRequired()])
+    ohp = DecimalField("ohp", validators=[validators.DataRequired()])
+    row = DecimalField("row", validators=[validators.DataRequired()])
 
 def get_columns():
     columns = []
@@ -19,6 +31,7 @@ def get_columns():
 
     return columns
 
+
 def get_primary_day(starting_rm5, exercise_type):
     weights_and_reps = [[], []]
     for k in reversed(range(4 + exercise_type)):
@@ -26,6 +39,7 @@ def get_primary_day(starting_rm5, exercise_type):
         weights_and_reps[1].append(5)
 
     return weights_and_reps
+
 
 def get_secondary_day(starting_rm5, exercise_type):
     weights_and_reps = get_primary_day(starting_rm5, exercise_type)
@@ -70,14 +84,23 @@ def get_madcow(squat_5rm, bench_5rm, dl_5rm, ohp_5rm, row_5rm):
 
     return program
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html', title='5x5')
+    form = RMForm(request.form)
 
-@app.route('/prog')
-def madcow():
-    c = get_columns()
+    if request.method == "POST":
+        squat = float(request.form["squat"])
+        bench = float(request.form["bench"])
+        dl = float(request.form["dl"])
+        ohp = float(request.form["ohp"])
+        row = float(request.form["row"])
 
-    p = get_madcow(150, 95, 190, 75, 90)
+    if form.validate_on_submit():
+        c = get_columns()
+        p = get_madcow(squat, bench, dl, ohp, row)
 
-    return render_template('prog.html', title="", data=p, columns=c)
+        return render_template('prog.html', data=p, columns=c)
+    else:
+        print(form.errors)
+
+    return render_template('index.html', title='5x5', form=form)
