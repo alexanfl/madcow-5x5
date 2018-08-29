652a8f69f3d9f3346f23a9abde95357a0f1deb99
Alexander Fleischer <fleisch91gmail.com>
2018-08-29 22:17:10 +0200

    Added cookies to save inputs and started navbar.

--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, flash
+from flask import Flask, render_template, jsonify, request, flash, make_response
 from flask_wtf import FlaskForm
 from wtforms import DecimalField
 from wtforms import validators
@@ -32,7 +32,8 @@ def get_columns():
     return columns
 
 
-def get_primary_day(starting_rm5, exercise_type):
+def get_primary_day(rm5, exercise_type):
+    starting_rm5 = rm5 - 10
     weights_and_reps = [[], []]
     for k in reversed(range(4 + exercise_type)):
         weights_and_reps[0].append(myround(starting_rm5*(1 - k*WEIGHT_INCREMENT)))
@@ -55,6 +56,7 @@ def get_secondary_day(starting_rm5, exercise_type):
 
 def get_12weeks(weights_and_reps):
     program = []
+
     i = 0
     for weight in weights_and_reps[0]:
         tmp_dict = {}
@@ -75,7 +77,13 @@ def myround(x, prec=1, base=WEIGHT_PRECISION):
     return round(base*round(float(x)/base), prec)
 
 
-def get_madcow(squat_5rm, bench_5rm, dl_5rm, ohp_5rm, row_5rm):
+def get_madcow(rm_5s):
+    squat_5rm = float(rm_5s["squat"]) 
+    bench_5rm = float(rm_5s["bench"])
+    dl_5rm = float(rm_5s["dl"])
+    ohp_5rm = float(rm_5s["ohp"])
+    row_5rm = float(rm_5s["row"])
+
     program = {}
 
     program["squat_primary"] = get_12weeks(get_primary_day(squat_5rm, 1))
@@ -84,22 +92,60 @@ def get_madcow(squat_5rm, bench_5rm, dl_5rm, ohp_5rm, row_5rm):
 
     return program
 
+
+def set_cookies_from_dict(rm_5s):
+    c = get_columns()
+    p = get_madcow(rm_5s)
+    resp = make_response(render_template('madcow.html', data=p, columns=c))
+
+    resp.set_cookie("squat", rm_5s["squat"])
+    resp.set_cookie("bench", rm_5s["bench"])
+    resp.set_cookie("dl", rm_5s["dl"])
+    resp.set_cookie("ohp", rm_5s["ohp"])
+    resp.set_cookie("row", rm_5s["row"])
+
+    return resp
+
+def get_cookies_as_dict():
+    rm_5s = dict(squat=request.cookies.get("squat"),
+                 bench=request.cookies.get("bench"),
+                 dl=request.cookies.get("dl"),
+                 ohp=request.cookies.get("ohp"),
+                 row=request.cookies.get("row")
+                 )
+
+    for key, val in rm_5s.items():
+        if val == None:
+            return None
+        else:
+            return rm_5s
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
+    rm_5s = get_cookies_as_dict()
+    if rm_5s:
+        c = get_columns()
+        p = get_madcow(rm_5s)
+
+        return render_template('madcow.html', data=p, columns=c)
+    else:
+        print("No cookies stored.")
+
     form = RMForm(request.form)
 
     if request.method == "POST":
-        squat = float(request.form["squat"])
-        bench = float(request.form["bench"])
-        dl = float(request.form["dl"])
-        ohp = float(request.form["ohp"])
-        row = float(request.form["row"])
+        rm_5s = dict(squat=request.form["squat"],
+                     bench=request.form["bench"],
+                     dl=request.form["dl"],
+                     ohp=request.form["ohp"],
+                     row=request.form["row"]
+                     )
 
     if form.validate_on_submit():
-        c = get_columns()
-        p = get_madcow(squat, bench, dl, ohp, row)
+        resp = set_cookies_from_dict(rm_5s)
+        return resp
 
-        return render_template('prog.html', data=p, columns=c)
     else:
         print(form.errors)
 
